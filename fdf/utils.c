9c3b4ad403f635ca298a1b253f058388075a0990
Schiffer <arnaud.dugain@orange.fr>
2023-03-14 09:56:55 +0100

    enter the matrix

--- fdf/utils.c
+++ fdf/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adugain <adugain@student.42.fr>            +#+  +:+       +#+        */
+/*   By: adugain <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 18:18:43 by adugain           #+#    #+#             */
-/*   Updated: 2023/03/13 16:55:45 by adugain          ###   ########.fr       */
+/*   Updated: 2023/03/14 09:56:13 by adugain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,29 @@
 
 typedef	struct s_matrix
 {
+	int	fd;
 	int	x;
 	int	y;
-	char	**tab;
+	int	**tab;
+	
 }	t_matrix;
 
-void	get_matrix_base(int fd, t_matrix *matrix)
+void	get_matrix_base(t_matrix *matrix, char *map)
 {
 	char	*line;
 	
-	line = get_next_line(fd);
+	matrix->fd = open(map, O_RDONLY);
+	line = get_next_line(matrix->fd);
 	matrix->x = wordcount(line, ' ') - 1;
+	//free(line);
 	while (line != NULL)
 	{
 		ft_printf("%s", line);
-		line = get_next_line(fd);
+		line = get_next_line(matrix->fd);
 		matrix->y++;
+		//free(line);
 	}
-	
+	close(matrix->fd);
 }
 
 void	create_matrix(t_matrix *matrix)
@@ -39,24 +44,32 @@ void	create_matrix(t_matrix *matrix)
 	int	i;
 
 	i = 0;
-	matrix->tab = malloc(sizeof(char *)* matrix->y);
+	matrix->tab = malloc(sizeof(int *)* matrix->y);
 	while(i < matrix->y)
 	{
-		matrix->tab[i] = malloc(sizeof(char) * matrix->x);
+		matrix->tab[i] = malloc(sizeof(int) * matrix->x);
 		i++;
 	}
+}
+
+void	fill_matrix(t_matrix *matrix, char *map)
+{
+	char *line;
+	char **data;
+
 }
 
 int	main(int ac, char **av)
 {
 	t_matrix	matrix;
-	int	fd;
 	
+	matrix.x = 0;
+	matrix.y = 0;
 	if (ac == 2)
-		fd = open(av[1], O_RDONLY);
+		get_matrix_base(&matrix, av[1]);
 	else
 		return(0);
-	get_matrix_base(fd, &matrix);
 	create_matrix(&matrix);
+	fill_matrix(&matrix, av[1]);
 	ft_printf("y:%d\nx:%d", matrix.y, matrix.x);
 }
