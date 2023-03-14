ed6667c949b836b17a6ee2c211194b5e2d36a269
Arnaud DUGAIN <adugain@e1r10p12.clusters.42paris.fr>
2023-03-14 17:46:44 +0100

    full fill tab and parse

--- fdf/utils.c
+++ fdf/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adugain <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: adugain <adugain@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 18:18:43 by adugain           #+#    #+#             */
-/*   Updated: 2023/03/14 09:56:13 by adugain          ###   ########.fr       */
+/*   Updated: 2023/03/14 17:45:39 by adugain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,84 @@ typedef	struct s_matrix
 	
 }	t_matrix;
 
+void    free_matrix(int **tab, t_matrix *matrix)
+{
+    int    i;
+
+    i = 0;
+    while (i < matrix->y)
+    {
+        free(tab[i]);
+        i++;
+    }
+    free(tab);
+}
+
+void	print_matrix(t_matrix *matrix)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	while (i < matrix->y)
+	{
+		j = 0;
+		while(j < matrix->x)
+		{
+			ft_printf("%d", matrix->tab[i][j]);
+			j++;
+		}
+		ft_printf("\n");
+		i++;
+	}
+}
+
+void	fill_matrix(t_matrix *matrix, char *map)
+{
+	int	i;
+	int	j;
+	char	*line;
+	char	**data;
+	
+	i = 0;
+	matrix->fd = open(map, O_RDONLY);
+	line = get_next_line(matrix->fd);
+	data = ft_split(line, ' ');
+	while (i < matrix->y)
+	{
+		j = 0;
+		while (j < matrix->x)
+		{
+			matrix->tab[i][j] = ft_atoi((char *)data[j]);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	get_wrecked(char *str)
+{
+	if (str[strlen(str) - 1] == '\n')
+		str[strlen(str) - 1] = '\0';
+	else
+		return ;
+}
+
 void	get_matrix_base(t_matrix *matrix, char *map)
 {
 	char	*line;
 	
 	matrix->fd = open(map, O_RDONLY);
 	line = get_next_line(matrix->fd);
-	matrix->x = wordcount(line, ' ') - 1;
-	//free(line);
+	get_wrecked(line);
+	matrix->x = wordcount(line, ' ');
+	free(line);
 	while (line != NULL)
 	{
-		ft_printf("%s", line);
 		line = get_next_line(matrix->fd);
+		free(line);
 		matrix->y++;
-		//free(line);
 	}
 	close(matrix->fd);
 }
@@ -44,19 +108,12 @@ void	create_matrix(t_matrix *matrix)
 	int	i;
 
 	i = 0;
-	matrix->tab = malloc(sizeof(int *)* matrix->y);
-	while(i < matrix->y)
+	matrix->tab = malloc(sizeof(int*) * matrix->y);
+	while (i < matrix->y)
 	{
 		matrix->tab[i] = malloc(sizeof(int) * matrix->x);
 		i++;
 	}
-}
-
-void	fill_matrix(t_matrix *matrix, char *map)
-{
-	char *line;
-	char **data;
-
 }
 
 int	main(int ac, char **av)
@@ -71,5 +128,7 @@ int	main(int ac, char **av)
 		return(0);
 	create_matrix(&matrix);
 	fill_matrix(&matrix, av[1]);
+	print_matrix(&matrix);
+	free_matrix(matrix.tab, &matrix);
 	ft_printf("y:%d\nx:%d", matrix.y, matrix.x);
 }
